deda5077aaf383fccedcc6e1f1039bb3fdac91b2
Rifat Ahmed <153188650+SAVAGE-143@users.noreply.github.com>
2024-10-28 00:06:23 +0600

    Update DUMP.py

--- DUMP.py
+++ DUMP.py
@@ -1,1 +1,8 @@
-import file
+import platform,os
+#####
+os.system("git pull")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import file
+elif bit == '32bit':
+    print('32 Bit is not working')
